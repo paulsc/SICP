866fe863261ae23dd9dd8657fa569c4790e6a154
Paul <hi@paulsc.com>
2025-05-22 13:50:26 +0300

    ex36-37

--- 2. Building Abstractions with Data/2.2 Hierarchical Data and the Closure Property/ex2.36.rkt
+++ 2. Building Abstractions with Data/2.2 Hierarchical Data and the Closure Property/ex2.36.rkt
@@ -16,6 +16,6 @@
 
 
 ;(define s '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
-;(accumulate-n + 0 s)
+;(accumulate-n + 0 s) ; '(22 26 30)
 
 (provide accumulate accumulate-n)
--- 2. Building Abstractions with Data/2.2 Hierarchical Data and the Closure Property/ex2.37.rkt
+++ 2. Building Abstractions with Data/2.2 Hierarchical Data and the Closure Property/ex2.37.rkt
@@ -21,9 +21,44 @@
 
 (matrix-*-vector m1 v1)
 
-;(define (transpose mat)
-;  (accumulate-n ⟨??⟩ ⟨??⟩ mat))
+; (accumulate-n op init seqs)
+; op takes n arguments, one each time from n seqs
+; seqs here are the rows of the matrix
+; this is confusing because this would return one value per column
+; this must mean that we need to return a list every time, each list is a row
+; 
+; 1 2 3                1 4 7
+; 4 5 6     ---->      2 5 8
+; 7 8 9                3 6 9
 ;
-;(define (matrix-*-matrix m n)
-;  (let ((cols (transpose n)))
-;    (map ⟨??⟩ m)))
+; in this example the first call to op would be with:
+; '(1 4 7) which is our new row, so op is identity
+; -- no actually it's cons, since the elements are passed as args, not as a list
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+;(define m1 '((1 2 3) (4 5 6) (7 8 9)))
+;(transpose m1)
+
+; dot product:
+;  | 1 2 3 |   |  7  8 |    | 58 64 |
+;  | 4 5 6 | * |  9 10 |  = |       |
+;              | 11 12 |
+; because 1*7 + 2*9 + 3*11 = 58
+; if we tranpose the 2nd matrix, the dot args become
+;  | 1 2 3 |   | 7  9 11 |
+;  | 4 5 6 |   | 8 10 12 |
+; then the first row of our result is
+; [ (matrix1row1 * matrix2row1) (matrix1row1 * matrix2row2) ]
+; etc..
+; (map (lambda (row) (...)) m) calls fn with one row of m each time (twice in our example)
+; and should return ( row*matrix2row1 row*matrix2row2 )
+; which is the same as vector ("row") * matrix ("transposed n") multiplication
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row) (matrix-*-vector cols row)) m)))
+
+;(define m1 '((1 2 3) (4 5 6)))
+;(define m2 '((7 8) (9 10) (11 12)))
+;(matrix-*-matrix m1 m2); '((58 64) (139 154))
